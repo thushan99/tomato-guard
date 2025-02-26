126799ab31e004c47fa019dea6adccb75cf39795
thushan99 <m.t.miuranga@gmail.com>
2025-02-26 12:56:17 +0530

    Initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+
+export default function Home() {
+    return (
+        <View >
+            <Text>Uba meka hdapan Nishu</Text>
+            <Text>Umbaaaaaaaaaaaaaaaaaa</Text>
+        </View>
+    );
+};
